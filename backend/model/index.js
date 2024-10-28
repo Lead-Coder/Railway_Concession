a305cd0de38a4cff627caf3102511d694614e85f
Enayut <vishvignair@gmail.com>
2024-10-28 14:20:02 +0530

    Added Realtionships

--- backend/model/index.js
+++ backend/model/index.js
@@ -24,4 +24,13 @@ db.concession = require("./concession.model.js")(sequelize, Sequelize);
 db.officer = require("./officer.model.js")(sequelize, Sequelize);
 db.student = require("./student.model.js")(sequelize, Sequelize);
 
+db.college.hasMany(db.student, { foreignKey: "college_id" });
+db.student.belongsTo(db.college, { foreignKey: "college_id" });
+
+db.student.hasMany(db.concession, { foreignKey: "uid" });
+db.concession.belongsTo(db.student, { foreignKey: "uid" });
+
+db.officer.hasMany(db.concession, { foreignKey: "approved_by" });
+db.concession.belongsTo(db.officer, { foreignKey: "approved_by" });
+
 module.exports = db;
