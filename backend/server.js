35a1a89593028af916454c8298730b04c1642ee0
Enayut <vishvignair@gmail.com>
2024-10-28 13:41:00 +0530

    Added Config Files For server

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/model");
+db.sequelize.sync();
+
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+// });
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to my application." });
+});
+
+require("./app/routes/profile.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
